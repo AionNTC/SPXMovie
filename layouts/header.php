2a5bdb085cc07b6feb086ccf044f986bf80cb029
NTC <rutchata12@gmail.com>
2020-12-01 13:57:12 +0700

    first commit

---
+++ layouts/header.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="logo-bar">
+        <div class="wrapper">
+            <img src="image/logo.png" alt="SPX Movie">
+        </div>
+    </div>
+    <div class="menu-bar">
+        <div class="wrapper">
+            <ul>
+                <li class="active" ><a href="#">หนัง</a></li>
+                <li><a href="#">อนิเมะทั้งหมด</a></li>
+                <li><a href="#">ติดต่อ | ขอหนัง</a></li>
+            </ul>
+            <div class="search">
+                <i class="fas fa-search"></i>
+            </div>
+        </div>
+    </div>
+</header>
